f99b7170f664968faff9f241312a0db4b13d913c
Andrej Tomas <atexodus7@gmail.com>
2021-01-10 21:21:50 +0100

    Izmenjena metoda contains(), dodat komparator za listu, dodata kraca metoda za sumu kilometraza, formatiranje.

--- vezba-stabla/src/StabloProgram.java
+++ vezba-stabla/src/StabloProgram.java
@@ -6,161 +6,178 @@ import java.util.List;
 import org.svetovid.Svetovid;
 
 class Stablo {
-    private static class Cvor {
-    	Automobil info;
-    	Cvor levo;
-    	Cvor desno;
-    	
-    	public Cvor(Automobil info, Cvor levo, Cvor desno) {
-    		this.info = info;
-    		this.levo = levo;
-    		this.desno = desno;
-    	}
-    	
-    	public Cvor kopija() {
-    		return new Cvor(info, levo, desno);
-    	}
-    }
+	private static class Cvor {
+		Automobil info;
+		Cvor levo;
+		Cvor desno;
 
-    private Cvor koren;
+		public Cvor(Automobil info, Cvor levo, Cvor desno) {
+			this.info = info;
+			this.levo = levo;
+			this.desno = desno;
+		}
 
-    // konstruktor za prazno stablo
-    public Stablo() {
-    	this.koren = null;
-    }
-    
-    public Stablo(Automobil auto) {
-    	this.koren = new Cvor(auto, null, null);
-    }
-    
-    protected Stablo(Cvor cvor) {
-    	this.koren = cvor;
-    }
-    
-    
-    // ------ dodati opisane metode ------------
-    
-    // ispisuje registracije svih automobila iz Novog Sada
-    public void ispisiNS() {
-    	System.out.println("Svi automobili iz Novog Sada: ");
-    	sviIzNs(koren);
-    }
-    
-    private void sviIzNs(Cvor cvor) {
-    	if (cvor == null) 
-    		return;
-    	
-    	if (cvor.info.getRegistracija().contains("NS")) {
-    		System.out.println(cvor.info.getRegistracija());
-    	}
-    	
-    	sviIzNs(cvor.levo);
-    	sviIzNs(cvor.desno);    	
-    }
-    
-    // vraca sumu kilometraza svih plavih automobila
-    public long presliPlavi() {
-    	Suma suma = new Suma();
-    	presliPlavi(koren, suma);
-    	System.out.println("Plavi automobili su presli: ");
-    	return suma.getSuma();
-    }
-    
-    private void presliPlavi(Cvor cvor, Suma suma) {
-    	if (cvor == null)
-    		return;
-    	
-    	if (cvor.info.getBoja().equals("Plava")) {
-    		suma.setSuma(cvor.info.getKilometraza());
-    	}
-    	
-    	presliPlavi(cvor.levo, suma);
-    	presliPlavi(cvor.desno, suma);
-    }
-    
+		public Cvor kopija() {
+			return new Cvor(info, levo, desno);
+		}
+	}
 
-    /*
-     * Pronalazi u stablu auto sa prosledjenom registracijom i
-     * kreira novo stablo u kojem pronadjeni auto ima za @param long km
-     * povecanu predjenu kilometrazu
-     */
-    public Stablo presaoJos(String registracija, long km) {
-    	Cvor korenNovog = kopijaCvora(koren, registracija, km);
-    	return new Stablo(korenNovog);
-    }
-    
-    private Cvor kopijaCvora(Cvor cvor, String reg, long km) {
-    	if (cvor == null) 
-    		return null;
-    	
-    	Cvor novi;
-    	if (cvor.info.getRegistracija().equals(reg)) {
-    		Automobil auto = new Automobil(cvor.info.getModel(), 
-    										cvor.info.getBoja(), 
-    											reg, (cvor.info.getKilometraza() + km));
-    		novi = new Cvor(auto, null, null);
-    	} else {
-    		novi = new Cvor(cvor.info, null, null);
-    	}
-    	 
-    	novi.levo = kopijaCvora(cvor.levo, reg, km);
-    	novi.desno = kopijaCvora(cvor.desno, reg, km);
-    	return novi;
-    }
-    
-    /*
-     * Vraca listu automobila koji imaju vecu kilometrazu od 
-     * svog roditelja i susednog cvora (bratskog)
-     */
-    public List<Automobil> sortiraniPlavi() {
-    	List<Automobil> lista = new ArrayList<Automobil>();
-    	listaPlavih(lista, koren, null);
-    	return lista;
-    }
-    
-    private void listaPlavih(List<Automobil> lista, Cvor tekuci, Cvor roditelj) {
-    	if (tekuci == null) 
-    		return;
-    	
-    	// prvo proverimo da li je auto plavi i da li ima roditelja
-    	if (tekuci.info.getBoja().equals("Plava") && roditelj != null) {
-    		// zatim proverimo da li tekuci cvor ima vecu kilometrazu
-    		// od roditelja ako nema onda ne proveravamo nista dalje
-    		if (tekuci.info.getKilometraza() > roditelj.info.getKilometraza()) {
-    			// gledamo da li nas tekuci cvor ima susednog i da li susedni zadovoljava uslov
-    			if (roditelj.levo != null && roditelj.levo.info.getKilometraza() < tekuci.info.getKilometraza()) {
-    				lista.add(tekuci.info);
-    			}
-    			if (roditelj.desno != null && roditelj.desno.info.getKilometraza() < tekuci.info.getKilometraza()) {
-    				lista.add(tekuci.info);
-    			}
-    			
-    		}
-    	}
-    	
-    	listaPlavih(lista, tekuci.levo, tekuci);
-    	listaPlavih(lista, tekuci.desno, tekuci);    	
-    }
-    
-    public boolean jeBST() {
-    	return jeBSTpom(koren, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-    
-    private boolean jeBSTpom(Cvor tekuci, long minKm, long maxKm) {
-    	if (tekuci == null)
-    		return true;
-    	
-    	if (tekuci.info.getKilometraza() < minKm || tekuci.info.getKilometraza() > maxKm) 
-    		return false;
-    	
-    	return (jeBSTpom(tekuci.levo, minKm, tekuci.info.getKilometraza()-1) &&
-    			jeBSTpom(tekuci.desno, tekuci.info.getKilometraza()+1, maxKm));
-    }
-    
-    
-    
-    // ------ po potrebi i pomocne metode, konstruktore, klase i sl
-    // ------ ne dodavati polja u klasu!
+	private Cvor koren;
+
+	// Konstruktor za prazno stablo
+	public Stablo() {
+		this.koren = null;
+	}
+
+	public Stablo(Automobil auto) {
+		this.koren = new Cvor(auto, null, null);
+	}
+
+	protected Stablo(Cvor cvor) {
+		this.koren = cvor;
+	}
+
+	// ------ dodati opisane metode ------------
+
+	// Ispisuje registracije svih automobila iz Novog Sada
+	public void ispisiNS() {
+		System.out.println("Svi automobili iz Novog Sada: ");
+		sviIzNs(koren);
+	}
+
+	private void sviIzNs(Cvor cvor) {
+		if (cvor == null)
+			return;
+
+		if (cvor.info.getRegistracija().startsWith("NS"))
+			System.out.println(cvor.info.getRegistracija() + " | " + cvor.info.getKilometraza());
+
+		sviIzNs(cvor.levo);
+		sviIzNs(cvor.desno);
+	}
+
+	// Vraća sumu kilometraza svih plavih automobila
+	public long presliPlavi() {
+		Suma suma = new Suma();
+		presliPlavi(koren, suma);
+		System.out.println("Plavi automobili su presli: ");
+		return suma.getSuma();
+	}
+
+	public long presliPlavi2() {
+		return presliPlavi(koren);
+	}
+
+	private void presliPlavi(Cvor cvor, Suma suma) {
+		if (cvor == null)
+			return;
+
+		if (cvor.info.getBoja().equals("Plava"))
+			suma.setSuma(cvor.info.getKilometraza());
+
+		presliPlavi(cvor.levo, suma);
+		presliPlavi(cvor.desno, suma);
+	}
+
+	// Alternativna verzija prethodne metode bez klase Suma
+	private long presliPlavi(Cvor cvor) {
+		if (cvor == null)
+			return 0;
+
+		if (cvor.info.getBoja().equals("Plava"))
+			return cvor.info.getKilometraza() + presliPlavi(cvor.levo) + presliPlavi(cvor.desno);
+
+		return presliPlavi(cvor.levo) + presliPlavi(cvor.desno);
+	}
+
+	/*
+	 * Pronalazi u stablu auto sa prosleđenom registracijom i kreira novo stablo u
+	 * kojem pronađeni auto ima za @param long km povećanu pređenu kilometražu
+	 */
+	public Stablo presaoJos(String registracija, long km) {
+		Cvor korenNovog = kopijaCvora(koren, registracija, km);
+		return new Stablo(korenNovog);
+	}
+
+	private Cvor kopijaCvora(Cvor cvor, String reg, long km) {
+		if (cvor == null)
+			return null;
+
+		/*
+		 * Ako je registracija aut. u tekućem čvoru jednaka sa prosleđenom onda pravimo
+		 * novi aut. sa povećanom kilometražom, ako nije onda auto. dobija vrednost iz
+		 * tekućeg čvora
+		 */
+		Automobil auto = cvor.info.getRegistracija().equals(reg)
+				? new Automobil(cvor.info.getModel(), cvor.info.getBoja(), reg, (cvor.info.getKilometraza() + km))
+				: cvor.info;
+
+		Cvor novi = new Cvor(auto, kopijaCvora(cvor.levo, reg, km), kopijaCvora(cvor.desno, reg, km));
+
+		return novi;
+	}
+
+	/*
+	 * Vraća sortiranu listu automobila koji imaju vecu kilometražu od svog
+	 * roditelja i susednog čvora (bratskog)
+	 */
+	public List<Automobil> sortiraniPlavi() {
+		List<Automobil> lista = new ArrayList<Automobil>();
+		listaPlavih(lista, koren, null);
+
+		Collections.sort(lista, new Comparator<Automobil>() {
+			@Override
+			public int compare(Automobil a1, Automobil a2) {
+				return (int) (a1.getKilometraza() - a2.getKilometraza());
+			}
+		});
+
+		return lista;
+	}
+
+	private void listaPlavih(List<Automobil> lista, Cvor tekuci, Cvor roditelj) {
+		if (tekuci == null)
+			return;
+
+		// Prvo proverimo da li je auto plavi i da li ima roditelja
+		if (tekuci.info.getBoja().equals("Plava") && roditelj != null) {
+			// zatim proverimo da li tekući čvor ima veću kilometražu od roditelja
+			if (tekuci.info.getKilometraza() > roditelj.info.getKilometraza()) {
+				/*
+				 * Gledamo da li naš tekući čvor ima susednog i da li susedni zadovoljava uslov.
+				 * Ako je tekući čvor taj koji proveravamo, to jest ako upoređujemo sa samim
+				 * sobom taj čvor onda neće biti dodat u listu jer će kilometraža biti ista
+				 */
+				if (roditelj.levo != null && roditelj.levo.info.getKilometraza() < tekuci.info.getKilometraza())
+					lista.add(tekuci.info);
+
+				if (roditelj.desno != null && roditelj.desno.info.getKilometraza() < tekuci.info.getKilometraza())
+					lista.add(tekuci.info);
+			}
+		}
+
+		listaPlavih(lista, tekuci.levo, tekuci);
+		listaPlavih(lista, tekuci.desno, tekuci);
+	}
+
+	// Vraća true ako je trenutno stablo, binarno stablo pretraživanja
+	public boolean jeBST() {
+		return jeBSTpom(koren, Long.MIN_VALUE, Long.MAX_VALUE);
+	}
+
+	private boolean jeBSTpom(Cvor tekuci, long minKm, long maxKm) {
+		if (tekuci == null)
+			return true;
+
+		if (tekuci.info.getKilometraza() < minKm || tekuci.info.getKilometraza() > maxKm)
+			return false;
+
+		return (jeBSTpom(tekuci.levo, minKm, tekuci.info.getKilometraza() - 1)
+				&& jeBSTpom(tekuci.desno, tekuci.info.getKilometraza() + 1, maxKm));
+	}
+
+	// ------ po potrebi i pomocne metode, konstruktore, klase i sl
+	// ------ ne dodavati polja u klasu!
 }
 
 // Glavna klasa
@@ -169,35 +186,29 @@ public class StabloProgram {
 	// Glavni program
 	public static void main(String[] args) {
 
-		// Napravimo pomocni objekat za ucitavanje i ispisivanje
+		// Napravimo pomoćni objekat za uitavanje i ispisivanje
 		TreeIO<Stablo> io = new TreeIO<>(Stablo.class);
 
-		// Procitamo stablo iz fajla
+		// Pročitamo stablo iz fajla
 		Stablo stablo = io.read(Svetovid.in("res/domaci9/Veliko.txt"));
-		// alternativno mozemo iz nekog drugog fajla
-		// otkomentarisati neki od redova dole, a skloniti ovaj iznad
-		//Stablo stablo = io.read(Svetovid.in("Veliko.txt"));
-		//Stablo stablo = io.read(Svetovid.in("Prazno.txt"));
 
 		// Ispisemo ucitano stablo
 		io.print(Svetovid.out, stablo);
 		System.out.println();
-		
-		
+
 		// -------- ovde dodati pozive metoda ----------
-		
+
 		stablo.ispisiNS();
-		System.out.println(stablo.presliPlavi());
+		System.out.println(stablo.presliPlavi() + " | " + stablo.presliPlavi2());
 		System.out.println();
-		Stablo novo = stablo.presaoJos("BG 082-ZD", 12000);
-		//io.print(Svetovid.out, novo);
-		//System.out.println();
+		// Stablo novo = stablo.presaoJos("BG 082-ZD", 12000);
+		// io.print(Svetovid.out, novo);
+
 		System.out.println(stablo.sortiraniPlavi());
-		
-		if(stablo.jeBST()) {
+
+		if (stablo.jeBST())
 			System.out.println("Stablo jeste BST.");
-		} else {
+		else
 			System.out.println("Stablo nije BST.");
-		}
 	}
 }
